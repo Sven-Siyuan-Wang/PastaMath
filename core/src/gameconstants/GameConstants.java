e3b89cf1a25adf6ca98f3e484bd2fd8b281fef40
Hazel Lau <Hazel Lau>
2016-03-27 16:18:20 +0800

    added bitmap font

--- core/src/gameconstants/GameConstants.java
+++ core/src/gameconstants/GameConstants.java
@@ -8,4 +8,7 @@ public class GameConstants {
     //for resizing screen
     public static float SCALE_X;
     public static float SCALE_Y;
+
+    //for fonts
+    public static final String FONT_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_!$%#@|\\/?-+=()*&.;,{}\"´`'<>";
 }
--- core/src/gameworld/GameRenderer.java
+++ core/src/gameworld/GameRenderer.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -36,6 +37,7 @@ public class GameRenderer {
     private int gameWidth;
     private float aspect_ratio;
 
+    private BitmapFont font;
 
     public GameRenderer(GameWorld world, int gameWidth, int gameHeight) {
         this.myWorld = world;
@@ -52,6 +54,7 @@ public class GameRenderer {
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setProjectionMatrix(cam.combined);
 
+        this.font = new BitmapFont(Gdx.files.internal("data/font.fnt"));
     }
 
     public void render(float runTime) {
@@ -91,6 +94,8 @@ public class GameRenderer {
         batcher.begin();
 
         batcher.enableBlending();
+        this.font.draw(batcher, "meow", 100, 100);
+
 
         //render background!
         batcher.draw(AssetLoader.gameBackground, 0, 0, 1280*GameConstants.SCALE_X, 720*GameConstants.SCALE_Y);
