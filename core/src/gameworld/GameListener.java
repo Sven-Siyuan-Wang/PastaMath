71441eec34e07b8960846671282689e75ff3741b
valerietantan <valerie_tan@mymail.sutd.edu.sg>
2016-04-15 18:03:28 +0800

    playing around using InstanceOf

---
+++ core/src/gameworld/GameListener.java
@@ -0,0 +1,133 @@
+//package gameworld;
+//
+//import com.badlogic.gdx.Game;
+//import com.badlogic.gdx.physics.box2d.Contact;
+//import com.badlogic.gdx.physics.box2d.ContactImpulse;
+//import com.badlogic.gdx.physics.box2d.ContactListener;
+//import com.badlogic.gdx.physics.box2d.Fixture;
+//import com.badlogic.gdx.physics.box2d.Manifold;
+//
+//import java.util.ArrayList;
+//
+//import gameobjects.Item;
+//import gameobjects.Player;
+//
+///**
+// * Created by valerie_tan on 4/9/2016.
+// */
+//public class GameListener implements ContactListener {
+//    /*
+//    The Contact Listeners listen for collisions events on a specific fixture.
+//    The methods are passed a Contact object, which contain information about the two bodies involved. The beginContact method is called when the object overlap another.
+//    When the objects are no longer colliding, the endContact method is called.
+// */
+//    //TODO: transfer all the gameworld update checking for different collisions into here
+//    //ANY 2 OBJECTS
+//
+//    //TODO: need to use inside the update() method? or it constantly listens?
+//
+//    //todo: IN ORDER TO change information, scores of players or get info from items
+//    //todo: cast the getUserData to respective objects first from Fixtures
+//    //todo: fixtures are updated over here, afterwhich actual objects will update their positions based on fixtures
+//
+//    //todo: determine what to update in Listener(everything else),
+//    //todo: what to update in GameWorld(set position only?- takes on the body/fixture's positon)
+//    private GameWorld gameWorld;
+//    private static ArrayList items;
+//    private static int player_body_to_remove= 0;
+//    private static int item_body_to_remove= 0;
+//
+//    public GameListener(GameWorld gameWorld) {
+//        this.gameWorld = gameWorld;
+//        items= gameWorld.simple_item_buffer.items_currently_appearing;
+//        //todo: actions to take for various collisions (make use of existing methods in the object alr)
+//
+//    }
+//        ArrayList<Player> holding_2_players = new ArrayList<Player>();
+//
+//        @Override
+//        public void beginContact (Contact contact)
+//        {
+//            Fixture fixtureA = contact.getFixtureA();
+//            Fixture fixtureB = contact.getFixtureB();
+//
+//            //ACTIONS FOR PLAYER COLLIDING
+//            if (players_collided(fixtureA, fixtureB)) {
+//                Player playerA = (Player) fixtureA.getUserData();
+//                Player playerB = (Player) fixtureB.getUserData();
+//                holding_2_players.add(playerA);
+//                holding_2_players.add(playerB);
+//                for (Player each_player : holding_2_players) {
+//                    if (each_player.getShielded()) {
+//                        each_player.update_collision_count();
+//                    } else {
+//                        each_player.deductChangingScoreUponKnock(gameWorld.deduction_score);
+//                    }
+//                    gameWorld.sendPlayerScore(each_player);
+//                }
+//            }
+//            //ACTIONS FOR PLAYER-ITEM OR ITEM-PLAYER COLLISION
+//            if (playerA_itemB(fixtureA, fixtureB)) {
+//                Player player = (Player) fixtureA.getUserData();
+//                Item item = (Item) fixtureB.getUserData();
+//                item.update_player_situation(player);
+//                //obtain index of item to remove its corresponding Body
+//                item_body_to_remove = items.indexOf(item);
+//                items.remove(item);
+//                gameWorld.sendRemoveItem(item);
+//                //todo: remove corresponding Body of item
+//                gameWorld.current_item_bodies.removeIndex(item_body_to_remove);
+//                gameWorld.sendPlayerScore(player);
+//            }
+//            if (itemA_playerB(fixtureA, fixtureB)) {
+//                Player player = (Player) fixtureB.getUserData();
+//                Item item = (Item) fixtureA.getUserData();
+//                item.update_player_situation(player);
+//
+//                items.remove(item);
+//                gameWorld.sendRemoveItem(item);
+//                gameWorld.sendPlayerScore(player);
+//            }
+//
+//        }
+//
+//        @Override
+//        public void endContact (Contact contact)
+//        {
+//
+//        }
+//
+//        //@Override
+//
+//    public void preSolve(Contact contact, Manifold oldManifold) {
+//        System.out.println("preSolve");
+//        //todo: implement for players- bounce off from each other
+//
+//    }
+//
+//    //@Override
+//    public void postSolve(Contact contact, ContactImpulse impulse) {
+//        System.out.println("postSolve");
+//        //todo: obtain collision impulse results
+//    }
+//
+//
+//    //todo: functions to check for what type of objects collided
+//    public boolean players_collided(Fixture a, Fixture b) {
+//        return (a.getUserData() instanceof Player && b.getUserData() instanceof Player);
+//    }
+//
+//
+//    //todo: make it more efficient
+//    public boolean playerA_itemB(Fixture a, Fixture b) {
+//        //checks if a player collected an item
+//        return (a.getUserData() instanceof Player && b.getUserData() instanceof Item);
+//    }
+//
+//    public boolean itemA_playerB(Fixture a, Fixture b) {
+//        return (a.getUserData() instanceof Item && b.getUserData() instanceof Player);
+//    }
+//
+//}
+//
+//
--- core/src/gameworld/WorldContactListener.java
+++ core/src/gameworld/WorldContactListener.java
@@ -6,7 +6,10 @@ import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.MyGdxGame;
+
+import java.util.ArrayList;
 
 import gameobjects.Item;
 import gameobjects.NumberAndOperand;
@@ -17,15 +20,22 @@ import gameobjects.Shield;
  * Created by WSY on 12/4/16.
  */
 public class WorldContactListener implements ContactListener {
+    //private static ArrayList items;
+    private static int player_body_to_remove= 0;
+    private static int item_body_to_remove= 0;
+
+
+    ArrayList<Player> holding_2_players = new ArrayList<Player>();
 
     final String TAG = "WorldContactListener";
     @Override
     public void beginContact(Contact contact) {
         Gdx.app.log(TAG, "beginContact");
         if(GameWorld.isOwner){
-            Fixture fixA = contact.getFixtureA();
-            Fixture fixB = contact.getFixtureB();
+            Fixture fixtureA = contact.getFixtureA();
+            Fixture fixtureB = contact.getFixtureB();
 
+            /*
             int cDef = fixA.getFilterData().categoryBits | fixB.getFilterData().categoryBits;
 
             switch (cDef){
@@ -46,6 +56,49 @@ public class WorldContactListener implements ContactListener {
                     }
 
             }
+            */
+            //ACTIONS FOR PLAYER COLLIDING
+            if (players_collided(fixtureA, fixtureB)) {
+                Player playerA = (Player) fixtureA.getUserData();
+                Player playerB = (Player) fixtureB.getUserData();
+                holding_2_players.add(playerA);
+                holding_2_players.add(playerB);
+                for (Player each_player : holding_2_players) {
+                    if (each_player.getShielded()) {
+                        each_player.update_collision_count();
+                    } else {
+                        each_player.decreaseScoreUponKnock();
+                    }
+                    //gameWorld.sendPlayerScore(each_player);
+                    //todo: reverse player
+                    each_player.b2body.applyForce(2.0f, 2.0f, each_player.getX(), each_player.getY(), true);
+                }
+
+                //todo: bouncing effect
+            }
+            //ACTIONS FOR PLAYER-ITEM OR ITEM-PLAYER COLLISION
+            if (playerA_itemB(fixtureA, fixtureB)) {
+                Player player = (Player) fixtureA.getUserData();
+                Item item = (Item) fixtureB.getUserData();
+                item.update_player_situation(player);
+                //obtain index of item to remove its corresponding Body
+                item_body_to_remove = GameWorld.items.indexOf(item);
+                GameWorld.items.remove(item);
+                //GameWorld.sendRemoveItem(item);
+                //todo: remove corresponding Body of item
+                //GameWorld.current_item_bodies.removeIndex(item_body_to_remove);
+                //GameWorld.sendPlayerScore(player);
+            }
+            if (itemA_playerB(fixtureA, fixtureB)) {
+                Player player = (Player) fixtureB.getUserData();
+                Item item = (Item) fixtureA.getUserData();
+                item.update_player_situation(player);
+
+                GameWorld.items.remove(item);
+                //gameWorld.sendRemoveItem(item);
+                //gameWorld.sendPlayerScore(player);
+            }
+
 
         }
 
@@ -67,5 +120,22 @@ public class WorldContactListener implements ContactListener {
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
 
+    }
+
+
+    //todo: functions to check for what type of objects collided
+    public boolean players_collided(Fixture a, Fixture b) {
+        return (a.getUserData() instanceof Player && b.getUserData() instanceof Player);
+    }
+
+
+    //todo: make it more efficient
+    public boolean playerA_itemB(Fixture a, Fixture b) {
+        //checks if a player collected an item
+        return (a.getUserData() instanceof Player && b.getUserData() instanceof Item);
+    }
+
+    public boolean itemA_playerB(Fixture a, Fixture b) {
+        return (a.getUserData() instanceof Item && b.getUserData() instanceof Player);
     }
 }
